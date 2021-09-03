a49db46f41fa9579d928d27d42dc31f79fb09d7f
Gerald Col <gnut@gnut.eu>
2021-09-03 14:53:51 +0200

    SQL request

--- app/Controllers/BlogController.php
+++ app/Controllers/BlogController.php
@@ -4,13 +4,20 @@ namespace App\Controllers;
 
 class BlogController extends Controller{
 
+    public function welcome() 
+    {
+        return $this->view('blog.welcome'); 
+    }
     public function index() 
     {
-        return $this->view('blog.index'); 
+        $stmat = $this->db->getPDO()->query('SELECT * FROM posts ORDER BY created_at DESC');
+        $posts = $stmat->fetchAll();
+        return $this->view('blog.index', compact('posts')); 
     }
 
     public function show(int $id) // put string if you don't want to have an error with id "a56" for example
     {
+        /*
         //$db = new DBConnection('myapp', 'localhost', 'root', '');
         //var_dump($db->getPDO());
         $req = $this->db->getPDO()->query('SELECT * FROM posts');
@@ -19,6 +26,10 @@ class BlogController extends Controller{
         foreach ($posts as $post) {
             echo $post->title;
         }
-        return $this->view('blog.show', compact('id')); //Create array containing variables and their values
+        */
+        $stmat = $this->db->getPDO()->prepare('SELECT * FROM posts WHERE id = ?');
+        $stmat->execute([$id]);
+        $post = $stmat->fetch();
+        return $this->view('blog.show', compact('post')); //Create array containing variables and their values
     }
 }
