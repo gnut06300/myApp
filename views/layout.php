4327c3f4e0f63337fe6a74a38b343553bd0d8231
Gerald Col <gnut@gnut.eu>
2021-09-09 15:14:41 +0200

    add a footer

--- views/layout.php
+++ views/layout.php
@@ -27,9 +27,9 @@
             </ul>
             <ul class="navbar-nav ml-auto">
                 <?php if (isset($_SESSION['auth'])) : ?>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?= REPERT ?>/logout">Se déconnecter</a>
-                </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= REPERT ?>/logout">Se déconnecter</a>
+                    </li>
                 <?php endif ?>
             </ul>
         </div>
@@ -37,9 +37,17 @@
     <div class="container">
         <?= $content ?>
     </div>
+    <footer class="bg-light py-4 mt-5">
+        <div class="container">
+            <div class="row">
+            <div class="col-12 col-sm-9 text-center">&copy; Copyright : <a href="https://gnut.eu" target="_blank">Gnut.eu</a> - 2021</div>
+            <div class="col-12 col-sm-3 text-center"><a href="<?= REPERT ?>/admin/posts">Administration</a></div>
+            </div>
+        </div>
+    </footer>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 
 </html>
